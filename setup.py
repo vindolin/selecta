7563c70bf3c15d0ea61d0bd6c44c5b9da7b9c35e
Thomas Schüßler <vindolin@gmail.com>
2016-01-08 09:29:04 +0100

    packaging

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='selecta',
-    version='0.0.11',
+    version='0.0.12',
     author='Thomas Schüßler',
     author_email='vindolin@gmail.com',
     packages=['selecta'],
