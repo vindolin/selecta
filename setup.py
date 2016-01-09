b5bf5b78f422ba5564fae8487b29815a0c444d05
Thomas Schüßler <vindolin@gmail.com>
2016-01-09 20:53:20 +0100

    add bash keyboard shortcut install

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='selecta',
-    version='0.0.13',
+    version='0.0.14',
     author='Thomas Schüßler',
     author_email='vindolin@gmail.com',
     packages=['selecta'],
-    scripts=['bin/selecta'],
+    scripts=['bin/selecta', 'bin/selecta_install_bash'],
     url='https://github.com/vindolin/selecta',
     license='MIT',
     description='Interactively select an entry from your bash/zsh history.',
