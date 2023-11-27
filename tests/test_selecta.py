18199a3976d08511253af9f4c5a00b425c919b51
Thomas Schüßler <vindolin@gmail.com>
2023-11-27 22:46:39 +0100

    typing...

--- tests/test_selecta.py
+++ tests/test_selecta.py
@@ -1,15 +1,14 @@
-from asyncio import FastChildWatcher
 import unittest
 from pathlib import Path
 from selecta import Selecta, mark_parts
 
 
 class TestSelecta(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super(TestSelecta, self).__init__(*args, **kwargs)
 
     def run_test(self, file, input, reverse_order=False, remove_bash_prefix=False, remove_zsh_prefix=False,
-                 regexp=False, case_sensitive=False, remove_duplicates=False, highlight_matches=False):
+                 regexp=False, case_sensitive=False, remove_duplicates=False, highlight_matches=False) -> Selecta:
 
         with open(Path(__file__).parent / 'data' / file, 'r') as fh:
             selecta = Selecta(
@@ -31,46 +30,46 @@ class TestSelecta(unittest.TestCase):
 
             return selecta
 
-    def test_words_default_rd(self):
+    def test_words_default_rd(self) -> None:
         selecta = self.run_test('test.txt', 'app bana', remove_duplicates=True)
         self.assertEqual(selecta.matching_line_count, 2)
 
-    def test_words_default_nrd(self):
+    def test_words_default_nrd(self) -> None:
         selecta = self.run_test('test.txt', 'app bana')
         self.assertEqual(selecta.matching_line_count, 3)
 
-    def test_words_case_sensitive(self):
+    def test_words_case_sensitive(self) -> None:
         selecta = self.run_test('test.txt', 'Orange', case_sensitive=True, remove_duplicates=True)
         self.assertEqual(selecta.matching_line_count, 1)
 
-    def test_baz(self):
+    def test_baz(self) -> None:
         selecta = self.run_test('test.txt', 'baz')
         self.assertEqual(selecta.matching_line_count, 0)
 
-    def test_regex_default(self):
+    def test_regex_default(self) -> None:
         selecta = self.run_test('test.txt', 'Or.+bana', regexp=True, case_sensitive=True, remove_duplicates=True)
         self.assertEqual(selecta.matching_line_count, 1)
 
-    def test_bash_prefix(self):
+    def test_bash_prefix(self) -> None:
         selecta = self.run_test('test_history.txt', r'^[^\d]+$', regexp=True, remove_bash_prefix=True, remove_duplicates=True)
         self.assertEqual(selecta.matching_line_count, 4)
 
-    def test_sentence(self):
+    def test_sentence(self) -> None:
         selecta = self.run_test('test.txt', '"orange cherry apple banana banana pe')
         self.assertEqual(selecta.matching_line_count, 1)
 
-    def test_empty_file(self):
+    def test_empty_file(self) -> None:
         selecta = self.run_test('test_empty.txt', 'foo')
         self.assertEqual(selecta.matching_line_count, 0)
 
-    def test_mark_parts1(self):
+    def test_mark_parts1(self) -> None:
         parts = mark_parts('orange cherry Orange apple Banana banana Pear apple', ['bana', 'apple', 'pear'], case_sensitive=False, highlight_matches=True)
         self.assertEqual(parts, ['orange cherry Orange ', ('match', 'apple'), ' ', ('match', 'Bana'), 'na ', ('match', 'bana'), 'na ', ('match', 'Pear'), ' ', ('match', 'apple')])
 
-    def test_mark_parts2(self):
+    def test_mark_parts2(self) -> None:
         parts = mark_parts('orange cherry Orange apple Banana banana Pear apple', ['cher', 'Bana'], case_sensitive=True, highlight_matches=True)
         self.assertEqual(parts, ['orange ', ('match', 'cher'), 'ry Orange apple ', ('match', 'Bana'), 'na banana Pear apple'])
 
-    def test_mark_parts3(self):
+    def test_mark_parts3(self) -> None:
         parts = mark_parts('apple orange cherry apple banana banana pear', ['pear', 'banana'], case_sensitive=True, highlight_matches=True)
         self.assertEqual(parts, ['apple orange cherry apple ', ('match', 'banana'), ' ', ('match', 'banana'), ' ', ('match', 'pear')])
