6a007a8e6723668753ddf19c65a63d0a15ab102d
Thomas Schüßler <vindolin@gmail.com>
2023-11-27 09:54:19 +0100

    add tests

---
+++ tests/tests.py
@@ -0,0 +1,46 @@
+import unittest
+from selecta import Selecta
+
+
+class TestSelecta(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestSelecta, self).__init__(*args, **kwargs)
+
+    def run_test(self, file, input, reverse_order=False, remove_bash_prefix=False, remove_zsh_prefix=False,
+                 regexp=False, case_sensitive=False, remove_duplicates=False, show_matches=False):
+
+        with open(file) as fh:
+            selecta = Selecta(
+                infile=fh,
+                reverse_order=reverse_order,
+                remove_bash_prefix=remove_bash_prefix,
+                remove_zsh_prefix=remove_zsh_prefix,
+                regexp=regexp,
+                case_sensitive=case_sensitive,
+                remove_duplicates=remove_duplicates,
+                show_matches=show_matches,
+                test_mode=True,
+            )
+
+            selecta.loop.start()
+            selecta.edit_change(None, input)
+            selecta.edit_done(None)
+            selecta.loop.stop()
+
+            return selecta
+
+    def test_words_default(self):
+        selecta = self.run_test('tests/test.txt', 'app bana', remove_duplicates=True)
+        self.assertEqual(selecta.matching_line_count, 2)
+
+    def test_words_case_sensitive(self):
+        selecta = self.run_test('tests/test.txt', 'Orange', case_sensitive=True, remove_duplicates=True)
+        self.assertEqual(selecta.matching_line_count, 1)
+
+    def test_regex_default(self):
+        selecta = self.run_test('tests/test.txt', 'Or.+bana', regexp=True, case_sensitive=True, remove_duplicates=True)
+        self.assertEqual(selecta.matching_line_count, 1)
+
+    def test_bash_prefix(self):
+        selecta = self.run_test('tests/test_history.txt', r'^[^\d]+$', regexp=True, remove_bash_prefix=True, remove_duplicates=True)
+        self.assertEqual(selecta.matching_line_count, 4)
